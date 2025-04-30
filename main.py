6dea469e7015187ed99af48ba817d0394ab6d2ee
Enis <enisyeager@gmail.com>
2025-04-30 11:07:38 +0200

    enis

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ button_start_at = 10
 button_color = "#cce"
 background_color = "lightblue"
 snake_head_color = "#605"
-snake_tail_color = "a37"
+snake_tail_color = "#a37"
 screen_size = button_amount_per_row*(button_pixel_size+button_gap_size) + 2 * button_start_at - button_gap_size
 
-def change_color(row, line, color):
-    buttons[row][line].config(bg=color)
+def change_color(coordinates, color):
+    buttons[coordinates[0]][coordinates[1]].config(bg=color)
 
 
 
@@ -39,14 +39,50 @@ for line in range(button_amount_per_row):
 
 time.sleep(1.5)
 
+print("hi 1.5")
+
 #Snake initialisieren
 snake_spawn_point = int(round(button_amount_per_row/2-0.5))
 snake_coordinates = [snake_spawn_point, snake_spawn_point]
-change_color(snake_coordinates[0], snake_coordinates[1], snake_head_color)
+change_color(snake_coordinates, snake_head_color)
 current_direction = "right"
 spawn_new_tail = True
-while True:
+tail_list = [snake_coordinates]
+def main_loop(spawn_new_tail):
+    print("anfang schleife")
+    root.after(300, main_loop)
+    tail_list.append(snake_coordinates)
+    if not spawn_new_tail:
+        tail_list.pop[0] 
+        
+    if current_direction == "right":
+        snake_coordinates[0] += 1
     
-    time.sleep(0.3)
+    elif current_direction == "left":
+        snake_coordinates[0] -= 1
 
-root.mainloop()
+    elif current_direction == "up":
+        snake_coordinates[1] += 1
+    
+    elif current_direction == "down":
+        snake_coordinates[1] -= 1
+
+    snake_coordinates[0] = snake_coordinates[0] % button_amount_per_row 
+    snake_coordinates[1] = snake_coordinates[1] % button_amount_per_row 
+
+    
+
+    change_color(snake_coordinates, snake_head_color)
+    for tail in tail_list:
+        change_color(tail, snake_tail_color)
+
+    print(snake_coordinates)
+    print(tail_list)
+
+    print("ende schleife")
+    
+    #spawn_new_tail = False 
+
+    root.mainloop()
+
+main_loop(spawn_new_tail)
